945342e790c384905d9574af0825c99807ca93b7
Rovanion Luckey <rovanion.luckey@gmail.com>
2025-10-14 11:22:09 +0200

    Migrate from legacy fact $::operatingsystem to $facts['os']['distro']['id']

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,13 +6,13 @@
 class pam_access::params {
 
   if (($facts['os']['family'] == 'RedHat') and (versioncmp($facts['os']['release']['full'], '5') >= 0)) or (
-    ($::operatingsystem == 'Amazon') and (versioncmp($facts['os']['release']['full'], '2') >= 0)
+    ($facts['os']['distro']['id'] == 'Amazon') and (versioncmp($facts['os']['release']['full'], '2') >= 0)
   ) {
     $enable_pamaccess_flags  = ['--enablelocauthorize', '--enablepamaccess']
     $disable_pamaccess_flags = ['--enablelocauthorize', '--disablepamaccess']
   } elsif ($facts['os']['family'] == 'Debian') and (
-    (($::operatingsystem == 'Debian') and (versioncmp($facts['os']['release']['full'], '7') >= 0)) or
-    (($::operatingsystem == 'Ubuntu') and (versioncmp($facts['os']['release']['full'], '10') >= 0))
+    (($facts['os']['distro']['id'] == 'Debian') and (versioncmp($facts['os']['release']['full'], '7') >= 0)) or
+    (($facts['os']['distro']['id'] == 'Ubuntu') and (versioncmp($facts['os']['release']['full'], '10') >= 0))
   ) {
     $enable_pamaccess_flags  = []
     $disable_pamaccess_flags = []
